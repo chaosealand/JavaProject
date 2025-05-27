7d5e76846687b2ad345a29b2bf327e95bc77e9cc
cow44 <cow44194@gmail.com>
2025-05-28 00:02:42 +0800

    C

--- src/utils/MouseTracker.java
+++ src/utils/MouseTracker.java
@@ -9,15 +9,21 @@ public class MouseTracker implements EventHandler<MouseEvent> {
     public static double CursorX ;
     public static double CursorY ;
     public static boolean leftPressed = false;
+    public static boolean rightPressed = false;
 
     @Override
     public void handle(MouseEvent E) {
         CursorX = E.getX();
         CursorY = E.getY();
-        if (E.getEventType() == MouseEvent.MOUSE_PRESSED) {
+        if (E.getEventType() == MouseEvent.MOUSE_PRESSED && E.getButton() == MouseButton.PRIMARY) {
             leftPressed = true;
-        } else if (E.getEventType() == MouseEvent.MOUSE_RELEASED) {
+        } else if (E.getEventType() == MouseEvent.MOUSE_RELEASED&& E.getButton() == MouseButton.PRIMARY) {
             leftPressed = false;
+        }
+        if (E.getEventType() == MouseEvent.MOUSE_PRESSED&& E.getButton() == MouseButton.SECONDARY){
+            rightPressed = true;
+        } else if (E.getEventType() == MouseEvent.MOUSE_RELEASED&& E.getButton() == MouseButton.SECONDARY) {
+            rightPressed = false;
         }
     }
 }
